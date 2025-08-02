8975b7629de295feba15010890f11acc08294acf
Neon-099 <Emmannatividad099@gmail.com>
2025-08-02 20:22:21 +0800

    ft: enhance Feedbacks and Tech components with testimonials and technology display

--- WebPortfolio/src/components/Loader.jsx
+++ WebPortfolio/src/components/Loader.jsx
@@ -10,10 +10,14 @@ const Loader = () => {
 
 const CanvasLoader = () => {
   return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="gray" />
-    </mesh>
+    <>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 5]} intensity={1} />
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="gray" />
+      </mesh>
+    </>
   )
 }
 
