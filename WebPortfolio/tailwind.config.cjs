a48fec4f69c496ebd085cfe42dc959dbfa408c2a
Neon-099 <Emmannatividad099@gmail.com>
2025-08-02 10:50:11 +0800

    add

---
+++ WebPortfolio/tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/herobg.png')",
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+  // Ensure custom CSS classes are not purged
+  safelist: [
+    'black-gradient',
+    'violet-gradient',
+    'green-pink-gradient',
+    'orange-text-gradient',
+    'green-text-gradient',
+    'blue-text-gradient',
+    'pink-text-gradient',
+    'canvas-loader'
+  ]
+};
