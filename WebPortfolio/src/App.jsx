f956239ca9138b67c3b960bc2b6e064db7dcb79c
Neon-099 <Emmannatividad099@gmail.com>
2025-08-03 15:03:34 +0800

    ft: integrate ThemeProvider in main.jsx and update App.jsx for theme-based styling

--- WebPortfolio/src/App.jsx
+++ WebPortfolio/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter} from 'react-router-dom'
 import { About, Contact, Experience, Feedbacks, Hero,
   Navbar, Tech, Works, StarsCanvas } from './components';
+import { useTheme } from './contexts/ThemeContext';
 function App() {
+
+  const [state] = useTheme();
+  const isDark = state.theme === 'dark';
 
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary min-h-screen'>
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center min-h-screen">
+      <div className={`relative z-0 min-h-screen ${isDark ? `bg-primary` : `bg-white`} `}>
+        <div className={`${isDark ? `bg-hero-pattern bg-cover bg-no-repeat bg-center min-h-screen ` : `bg-white`}`}>
           <Navbar />
           <Hero />
         </div>
--- WebPortfolio/src/main.jsx
+++ WebPortfolio/src/main.jsx
@@ -2,10 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './tailwind.css'
+import { ThemeProvider } from './contexts/ThemeContext.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
+
+
