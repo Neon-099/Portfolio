eddebd45b9821c4403816cc101e25336092c6bda
Neon-099 <Emmannatividad099@gmail.com>
2025-08-03 23:25:32 +0800

    toggle theme

--- WebPortfolio/src/components/Experience.jsx
+++ WebPortfolio/src/components/Experience.jsx
@@ -9,8 +9,13 @@ import {experiences } from '../constants'
 import { SectionWrapper } from '../hoc/SectionWrapper';
 import { textVariant } from '../utils/motion'; 
 
+import { useTheme } from '../contexts/ThemeContext';
 
-const ExperienceCard = ( {experiences} ) => (
+
+
+const ExperienceCard = ( {experiences, theme} ) => (
+  
+
   <VerticalTimelineElement 
     contentStyle={{background: "#1d1836", color: "#fff"}}
     contentArrowStyle={{borderRight: '7px solid #232631'}}
@@ -24,14 +29,14 @@ const ExperienceCard = ( {experiences} ) => (
     }>
       {/**/}
       <div>
-        <h3 className='text-white text-[24px] font-bold'>{experiences.title}</h3>
+        <h3 className={theme === 'light' ? 'text-gray-200 text-[24px] font-bold' : 'text-white text-[24px] font-bold'}>{experiences.title}</h3>
         <p className='text-secondary text-[16px] font-semibold' style={ {margin:0}}> {experiences.company_name}</p>
       </div>
 
       <ul className='mt-5 list-disc ml-5 space-y-2 '>
         {experiences.points.map((points, index) => (
           <li key={`experiences-points-${index}`}
-              className='text-white-100 text-[14px] pl-1 tracking-wider'>
+              className={theme === 'light' ? 'text-gray-300 text-[14px] pl-1 tracking-wider' : 'text-white-100 text-[14px] pl-1 tracking-wider'}>
                 {points}
           </li>
         ))}
@@ -41,18 +46,30 @@ const ExperienceCard = ( {experiences} ) => (
 
 
 const Experience = () => {
+  
+  const [state] = useTheme();
+
   return (
     <>
       <motion.div 
         variants={textVariant()}> {/*variants: make P tags animate*/}
-        <p className={`${styles.sectionSubText}`}>What I have done so far</p>
-        <h2 className={`${styles.sectionHeadText}`}>Work Experience</h2>
+        <p className={`${styles.sectionSubText} ${state.theme === 'light' ? `text-gray-800` : `text-[#dfd9ff]`}`}>What I have done so far</p>
+        <h2 className={`${styles.sectionHeadText} ${state.theme === 'light' ? 'text-black-100' : 'text-white'}`}>Work Experience</h2>
       </motion.div>
 
       <div className='mt-20 flex flex-col'>
-        <VerticalTimeline>
+        <VerticalTimeline
+          lineColor={state.theme === 'light' ? '#e5e7eb' : '#fff'}
+        >
+          <style>
+            {`
+              .vertical-timeline-element-icon {
+                border: ${state.theme === 'light' ? '4px solid #374151' : ''} !important;
+              }
+            `}
+          </style>
           {experiences.map((exp, index) => (
-            <ExperienceCard key={index} experiences={exp} />
+            <ExperienceCard key={index} experiences={exp} theme={state.theme} />
           ))}  
         </VerticalTimeline>
       </div>
