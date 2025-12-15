9f82367c73f205bd65ae4f6dbb6c91c371359492
Neon-099 <Emmannatividad099@gmail.com>
2025-12-15 22:55:42 +0800

    change from main to page

--- MyPortfolio/src/page.tsx
+++ MyPortfolio/src/page.tsx
@@ -4,13 +4,17 @@ import './index.css'
 import Hero from './layout/Hero.tsx'
 import Header from './components/NavBar.tsx'
 import Technologies from './layout/Technologies.tsx'
+import MyProjects from './layout/MyProjects.tsx'
+import ContactCTA from './layout/ContactCTA.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <main className='pt-20'>
     <div> 
       <Header />
       <Hero />
       <Technologies />
+      <MyProjects />
+      <ContactCTA />
     </div>
-  </StrictMode>,
+  </main>,
 )
